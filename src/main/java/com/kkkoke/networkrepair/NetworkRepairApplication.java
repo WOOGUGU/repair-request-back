689271b1e451696c2631e3ceeda8505d6e03282f
kkkoke <1120746320@qq.com>
2021-11-15 18:12:33 +0800

    项目建立

---
+++ src/main/java/com/kkkoke/networkrepair/NetworkRepairApplication.java
@@ -0,0 +1,13 @@
+package com.kkkoke.networkrepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkRepairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkRepairApplication.class, args);
+    }
+
+}
