f92b9f41e9b54424140cd0c3834c911410e48418
kkkoke <1120746320@qq.com>
2021-11-22 01:35:37 +0800

    picker选择栏初步实现2

---
+++ src/main/java/com/kkkoke/networkrepair/dao/PickerTimeDao.java
@@ -0,0 +1,14 @@
+package com.kkkoke.networkrepair.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PickerTimeDao {
+    // 增加报修时间段
+
+    // 删除报修时间段
+
+    // 查找所有时间段
+
+    // 修改报修时间段
+}
