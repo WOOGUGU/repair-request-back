8574f42c7f55f70f9e26d9ce7fcba717d97ecfba
kkkoke <1120746320@qq.com>
2021-12-07 14:45:47 +0800

    小调整

--- src/main/java/com/kkkoke/networkrepair/NetworkRepairApplication.java
+++ src/main/java/com/kkkoke/networkrepair/NetworkRepairApplication.java
@@ -3,11 +3,12 @@ package com.kkkoke.networkrepair;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SpringBootApplication
 public class NetworkRepairApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NetworkRepairApplication.class, args);
     }
-
 }
