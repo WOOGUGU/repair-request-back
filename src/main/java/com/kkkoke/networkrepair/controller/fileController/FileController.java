26daf90eb14f71e9096889894ca959b732e24807
WOOGUGU <woogugu@outlook.com>
2022-05-07 16:18:34 +0800

    设置用户文件夹

--- src/main/java/com/kkkoke/networkrepair/controller/fileController/FileController.java
+++ src/main/java/com/kkkoke/networkrepair/controller/fileController/FileController.java
@@ -1,9 +1,11 @@
 package com.kkkoke.networkrepair.controller.fileController;
 
+import com.kkkoke.networkrepair.pojo.helper.UserResult;
 import com.kkkoke.networkrepair.result.ApiResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,9 +38,11 @@ public class FileController {
     @PostMapping(value = "/fileUpload")
     public Object fileUpload(
             @RequestParam(value = "file") MultipartFile file,
-            @RequestParam(value = "request") HttpServletRequest request
+            HttpServletRequest request
     ) {
         HttpSession session = request.getSession();
+        SecurityContext securityContext = (SecurityContext) session.getAttribute("SPRING_SECURITY_CONTEXT");
+        UserResult user = (UserResult) securityContext.getAuthentication().getPrincipal();
 
         if (file.isEmpty()) {
             log.error("无图片");
@@ -49,9 +53,8 @@ public class FileController {
         assert fileName != null;
         String suffixName = fileName.substring(fileName.lastIndexOf("."));
         // 新文件名
-        fileName = "/" + session.getAttribute("username") + "/" + UUID.randomUUID() + suffixName;
+        fileName = "/" + user.getUsername() + "/" + UUID.randomUUID() + suffixName;
         // 上传后的路径
-//        String filePath = System.getProperty("user.dir") + "/images/" + fileName;
         String filePath = imageHome + fileName;
         File dest = new File(filePath);
         if (!dest.getParentFile().exists()) {
@@ -60,26 +63,31 @@ public class FileController {
         try {
             file.transferTo(dest);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error(String.valueOf(e));
+            return ApiResult.fail("500", "上传失败", String.valueOf(e));
         }
         log.info("保存图片至==>" + filePath);
         //返回图片名称
-        return ApiResult.success(fileName, "获取成功");
+        return ApiResult.success(fileName, "上传成功");
     }
 
     @ApiOperation(value = "传多张图片")
     @PostMapping("/filesUpload")
     public Object filesUpload(
-            @RequestParam(value = "files") MultipartFile[] files
+            @RequestParam(value = "files") MultipartFile[] files,
+            HttpServletRequest request
     ) {
+        HttpSession session = request.getSession();
+        SecurityContext securityContext = (SecurityContext) session.getAttribute("SPRING_SECURITY_CONTEXT");
+        UserResult user = (UserResult) securityContext.getAuthentication().getPrincipal();
+
         List<String> list = new ArrayList<>();
         String fileName, suffixName, filePath;
         for (int i = 0; i < files.length; i++) {
             fileName = files[i].getOriginalFilename();
             assert fileName != null;
             suffixName = fileName.substring(fileName.lastIndexOf("."));
-            fileName = UUID.randomUUID() + suffixName;
-//            filePath = System.getProperty("user.dir") + "/images/" + fileName;
+            fileName = "/" + user.getUsername() + "/" + UUID.randomUUID() + suffixName;
             filePath = imageHome + fileName;
             java.io.File dest = new java.io.File(filePath);
             if (!dest.getParentFile().exists()) {
@@ -88,10 +96,11 @@ public class FileController {
             try {
                 files[i].transferTo(dest);
             } catch (IOException e) {
-                e.printStackTrace();
+                log.error(String.valueOf(e));
+                return ApiResult.fail("500", "上传失败", String.valueOf(e));
             }
             log.info("保存图片至==>" + filePath);
-            list.add("/" + fileName);
+            list.add(fileName);
         }
         return ApiResult.success(list, "获取成功");
     }
