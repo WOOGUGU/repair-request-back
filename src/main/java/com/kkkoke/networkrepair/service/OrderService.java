bb01a3a425ac967764efeb37cfdffd7331e2b490
kkkoke <1120746320@qq.com>
2022-05-18 22:38:40 +0800

    新增工单时，多图片上传，bug修复

--- src/main/java/com/kkkoke/networkrepair/service/OrderService.java
+++ src/main/java/com/kkkoke/networkrepair/service/OrderService.java
@@ -28,7 +28,8 @@ public interface OrderService {
     // 修改报修工单
     Order updateOrder(Integer orderId, String username, String sender, String tel, String type,
                       String des, String position, String timeSubscribe, Integer progress,
-                      String solver, String timeStart, String timeDistribution, String timeEnd, String feedback, Integer stars) throws DataHasNotExistedException;
+                      String solver, String timeStart, String timeDistribution, String timeEnd,
+                      String feedback, Integer stars, String imgPath) throws DataHasNotExistedException;
 
     // 审核工单
     Integer checkOrder(Integer orderId, Integer progress, String remark) throws DataHasNotExistedException;
